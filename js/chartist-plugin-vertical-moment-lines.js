a6f251c2e06210f03c419e8f8c45aa9cc923af49
Michaël Parienti <aleph-g@parienti.net>
2016-12-18 18:28:44 +0100

    WIP: Add Days & Nights

--- js/chartist-plugin-vertical-moment-lines.js
+++ js/chartist-plugin-vertical-moment-lines.js
@@ -26,25 +26,14 @@
     'use strict';
 
     var defaultOptions = {
-      positions: undefined,
-      labels: undefined,
-      classes: undefined,
+      lines: undefined,
+      rectangles: undefined,
       className: 'vt-mnt-line'
     };
 
-    var VerticalLine = function (chart, chartRect, options) {
+    var VerticalZone = function (chart, chartRect, data) {
 
-      // var labelClassName = options.className + '-label';
-      // var $label = $('.' + labelClassName);
-
-      // if (!$label.length) {
-      //   $label = $('<span class="' + labelClassName + '" style="position: absolute"></span>')
-      //     .appendTo(chart.container);
-      // }
-
-      // $('.' + labelClassName).hide();
-
-      this.show = function (x, className) {
+      this.showline = function (x, className) {
 
         // $label
         //   .html(options.label || '')
@@ -56,6 +45,18 @@
           x2: x,
           y1: chartRect.y1,
           y2: chartRect.y2// + $label.height()
+        }, className);
+      };
+      this.showrect = function (value, className) {
+        var xmin = Math.min(data.axisX.projectValue(value[0].getTime()), data.axisX.projectValue(value[2].getTime()));
+        var ymin = Math.min(value[1], value[3]);
+        var xmax = Math.max(data.axisX.projectValue(value[0].getTime()), data.axisX.projectValue(value[2].getTime()));
+        var ymax = Math.max(value[1], value[3]);
+        chart.svg.elem('rect', {
+          x: chartRect.x1 + xmin,
+          y: chartRect.y2, //value[1] if not NaN
+          width: xmax-xmin,//,
+          height: chartRect.y1 - chartRect.y2 // value[3] if not NaN
         }, className);
       };
     };
@@ -72,13 +73,19 @@
         }
 
         chart.on('created', function (data) {
-          var verticalline = new VerticalLine(chart, data.chartRect, options);//all options is useless
+          var verticalzone = new VerticalZone(chart, data.chartRect, data);
           var x = 0;
           var className = '';
-          for (var i=0; i<options.positions.length; i++) {
-            className = (options.classes[i] != undefined) ? options.classes[i] : options.className;
-            if ( (x = data.axisX.projectValue(options.positions[i].getTime())) > 0) {
-              verticalline.show(data.chartRect.x1 + x, className);
+          for (var i=0; i<options.lines.length; i++) {
+            className = options.lines[i].className;
+            if ( (x = data.axisX.projectValue(options.lines[i].value.getTime())) > 0) {//this test should be include in plugin
+              verticalzone.showline(data.chartRect.x1 + x, className);
+            }
+          }
+          for (var i=0; i<options.rectangles.length; i++) {
+            className = options.rectangles[i].className;
+            if ( (x = data.axisX.projectValue(options.rectangles[i].value[0].getTime())) > 0) {//this test should be include in plugin
+              verticalzone.showrect(options.rectangles[i].value, className);
             }
           }
         });
